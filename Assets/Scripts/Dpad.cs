c1dba068008b661ed4309032630c26f70ba9be90
YuujiNope <bluewingedfairyofthepapermoon@yahoo.com>
2016-08-23 19:27:19 +0700

    Add movement UI

---
+++ Assets/Scripts/Dpad.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Dpad : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+
+	public RectTransform button;
+	public PlayerMovement player;
+
+	int moveRange = 80;
+	int baseX;
+	int baseY;
+	string horizontalAxis = "Horizontal";
+	string verticalAxis = "Vertical";
+	bool isPointerDown = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		baseX = (int)button.position.x;
+		baseY = (int)button.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(isPointerDown){
+			UpdatePlayer ();
+		}
+	}
+
+	public void OnDrag (PointerEventData data)
+	{
+		UpdateButtonPosition (data);
+	}
+
+	public void OnPointerUp (PointerEventData data)
+	{
+		isPointerDown = false;
+		button.anchoredPosition = Vector2.zero;
+		player.Stop ();
+	}
+
+	public void OnPointerDown (PointerEventData data)
+	{
+		isPointerDown = true;
+		UpdateButtonPosition (data);
+	}
+
+	private void UpdateButtonPosition (PointerEventData data)
+	{
+		Vector2 newPos = Vector2.zero;
+		int delta = (int)(data.position.x - baseX);
+		delta = Mathf.Clamp (delta, -moveRange, moveRange);
+		newPos.x = delta;
+		delta = (int)(data.position.y - baseY);
+		delta = Mathf.Clamp (delta, -moveRange, moveRange);
+		newPos.y = delta;
+		button.anchoredPosition = newPos;
+	}
+
+	private void UpdatePlayer(){
+		if (button.anchoredPosition.x < 0)
+			player.MoveLeft (-button.anchoredPosition.x / 80);
+		else if(button.anchoredPosition.x > 0)
+			player.MoveRight (button.anchoredPosition.x / 80);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	float moveSpeed = 5f;
+	float jumpHeight = 8f;
+	Rigidbody2D rigid;
+	bool right;
+	bool left;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	//	void OnCollisionEnter2D(Collision2D collision){
+	//		Collider2D collider = collision.collider;
+	//		if(collider.tag == "World"){
+	//			Vector3 contact = collision.contacts [0].point;
+	//			Vector3 center = collider.bounds.center;
+	//			right = contact.x > center.x;
+	//			left = contact.x < center.x;
+	//			Debug.DrawLine (contact, center, Color.blue);
+	//		}
+	//	}
+
+	public void MoveRight (float multiplier)
+	{
+		CheckCollision ();
+		if (!right)
+			rigid.velocity = new Vector2 (moveSpeed * multiplier, rigid.velocity.y);
+		transform.localScale = new Vector3(1, 1, 1);
+	}
+
+	private void CheckCollision ()
+	{
+		right = Physics.Linecast (transform.position, transform.position + Vector3.right);
+	}
+
+	public void MoveLeft (float multiplier)
+	{
+		CheckCollision ();
+		if (!left)
+			rigid.velocity = new Vector2 (-moveSpeed * multiplier, rigid.velocity.y);
+		transform.localScale = new Vector3(-1, 1, 1);
+	}
+
+	public void Jump ()
+	{
+		if (rigid.velocity.y == 0) {
+			rigid.velocity = new Vector2 (rigid.velocity.x, jumpHeight);
+		}
+	}
+
+	public void Stop ()
+	{
+		rigid.velocity = new Vector2 (0, rigid.velocity.y);
+	}
+}
